16d6814431bd51d9b4042cc86dea764dd18937ab
mwihoti <danielmwihoti@gmail.com>
2026-07-08 16:55:40 +0300

    fix 02

--- submission/02.sh
+++ submission/02.sh
@@ -1,2 +1,2 @@
 # Create a native segwit address and get the public key from the address.
-bitcoin-cli -regtest getnewaddress address_type=p2sh-segwit
+bitcoin-cli -regtest getaddressinfo $(bitcoin-cli -regtest getnewaddress "" address_type=bech32) | jq -r '.pubkey'
