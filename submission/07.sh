c1cf77a683731d74809a58724a8cb4290c36512d
mwihoti <danielmwihoti@gmail.com>
2026-07-09 11:11:12 +0300

    07

--- submission/07.sh
+++ submission/07.sh
@@ -1,2 +1,2 @@
 # What is the total output value of this partially signed transaction in satoshis
-# transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+bitcoin-cli -regtest decodepsbt "cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA" | jq -r '[.tx.vout[].value | . * 1e8 | round] | add'
